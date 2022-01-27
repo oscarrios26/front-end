eb9b01cb961bca829c3af4d75867a9d597e21809
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2022-01-27 14:09:27 -0600

    finished navbar and css

--- my-app/src/components/Nav/Nav.jsx
+++ my-app/src/components/Nav/Nav.jsx
@@ -3,9 +3,16 @@ import './Nav.css'
 import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
-  return <div className='nav'>
-    <NavLink to='/'> Home </NavLink>
-    <NavLink to='/sign-up'> Sign Up </NavLink>
-    <NavLink className="nav-link" to='/sign-in'> Sign In </NavLink>
-  </div>;
+  return (
+    <div className='navContainer'>
+      <h2 id='title'>CarInfo.com</h2>
+      <div className='nav'>
+  
+      <NavLink className="nav-link" to='/'> Home </NavLink>
+      <NavLink className="nav-link" to='/sign-up'> Sign Up </NavLink>
+      <NavLink className="nav-link" to='/sign-in'> Sign In </NavLink>
+      </div>
+    </div>
+      
+    );
 }
