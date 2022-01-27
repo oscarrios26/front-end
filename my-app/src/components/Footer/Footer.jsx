70f7b81ada6f2ed8ea623274f9c829c92391502b
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2022-01-27 14:26:54 -0600

    finished footer and made it into a link to my gibhub

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css'
 
+
 export default function Footer() {
   return(
   <div className='footer'>
-        <h2>hey</h2>
+      <a className='githublink' href='https://github.com/oscarrios26'><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvypaTxnXP1gCEIYCUhywzJeg5uWglR7US51hqTrhXOkxOFTFUPTMegnhcw-06w3nKAAs&usqp=CAU'
+      width='60px'/></a>
   </div>
   );
 }
